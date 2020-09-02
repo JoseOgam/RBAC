ac396e0c75cae38b345c718c3db571fde2a589a7
JoseOgam <jotienoochieng@gmail.com>
2020-09-02 13:23:09 -0400

    user model created

---
+++ server/models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    default: 'basic',
+    enum: ['basic', 'supervisor', 'admin']
+  },
+  accessToken: {
+    type: String
+  }
+})
+
+const User = mongoose.model('user', userSchema)
+module.exports = User
